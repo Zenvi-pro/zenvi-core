fb32b0cbb1d4e5cf8b952a8ffcae0080f552b42b
Harkit2004 <hschhabra2@myseneca.ca>
2026-08-14 13:38:09 -0400

    chore: version bump to v1.0.183

--- src/classes/info.py
+++ src/classes/info.py
@@ -29,7 +29,7 @@ import os
 import sys
 from time import strftime
 
-VERSION = "1.0.182"
+VERSION = "1.0.183"
 # 0.3.2 minimum for systems where only stable PPA (or older) is available (e.g. aarch64).
 MINIMUM_LIBOPENSHOT_VERSION = "0.3.2"
 DATE = "20260813000000"
